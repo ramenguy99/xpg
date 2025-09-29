a68bbe840a609aa2c3535aba94655dcffab090a0
Dario Mylonopoulos <ramenguy99@gmail.com>
2025-09-29 22:53:24 +0200

    ambra: wip materials

--- ambra/ambra/materials.py
+++ ambra/ambra/materials.py
@@ -1,80 +1,64 @@
 # Copyright Dario Mylonopoulos
 # SPDX-License-Identifier: MIT
 
-from typing import List, Optional, Union
+from typing import List, Optional, Union, TypeAlias
 
 import numpy as np
 
 from .property import BufferProperty, ImageProperty, as_buffer_property, as_image_property
+from .utils.gpu import get_format_info
 
+MaterialData: TypeAlias = Union[np.ndarray, BufferProperty, ImageProperty]
 
-class ValueMaterialProperty:
-    def __init__(self, value: Union[BufferProperty, np.ndarray]):
-        self.value = as_buffer_property(value, np.float32, (-1,))
-
-
-class TextureMaterialProperty:
-    def __init__(self, image: Union[ImageProperty, np.ndarray]):
-        self.image = as_image_property(image)
-
+# TODO: rename diffuse to diffuse_color and specular to specular_color. Albedo maybe fine?
 
 class Material:
-    def __init__(self, properties: List[Union[ValueMaterialProperty, TextureMaterialProperty]]):
-        pass
+    def __init__(self, properties: List[Union[BufferProperty, ImageProperty]]):
+        self.properties = properties
 
 
-def check_material_property(
-    property: Union[ValueMaterialProperty, TextureMaterialProperty], channels: int
-) -> Union[ValueMaterialProperty, TextureMaterialProperty]:
-    if isinstance(property, ValueMaterialProperty):
-        # if property.value.shape
-        pass
-    elif isinstance(property, TextureMaterialProperty):
-        pass
+def as_material_property(
+    property: MaterialData, channels: int, name: str
+) -> MaterialData:
+    if isinstance(property, ImageProperty):
+        img = as_image_property(property, name=name)
+        info = get_format_info(img.format)
+        if info.channels < channels:
+            raise ValueError(f"Material property \"{name}\" image must have at least {channels}, but {img.format} only has {info.channels}")
     else:
-        raise TypeError(f"Unhandled type: {type(property)}")
-    return property
+        return as_buffer_property(property, np.float32, (channels,), name=name)
 
-
+# Unlit flat color
 class BaseColorMaterial(Material):
-    def __init__(self, color: Union[ValueMaterialProperty, TextureMaterialProperty]):
-        self.color = check_material_property(color, 3)
+    def __init__(self, color: MaterialData):
+        self.color = as_material_property(color, 3, "color")
         super().__init__([color])
 
-
+# Diffuse only
 class DiffuseMaterial(Material):
-    def __init__(self, albedo: Union[ValueMaterialProperty, TextureMaterialProperty]):
-        self.albedo = check_material_property(albedo, 3)
-        super().__init__([albedo])
+    def __init__(self, diffuse: MaterialData):
+        self.diffuse = as_material_property(diffuse, 3, "diffuse")
+        super().__init__([self.diffuse])
 
+# Blinn-phong
+class DiffuseSpecularMaterial(Material):
+    def __init__(self,
+                 diffuse: MaterialData,
+                 specular: MaterialData,
+                 ):
+        self.diffuse = as_material_property(diffuse, 3, "diffuse")
+        self.specular = as_material_property(diffuse, 3, "specular")
+        super().__init__([diffuse, specular])
 
-class RoughnessMetallicMaterial(Material):
+# PBR metallic roughness
+class PBRMaterial(Material):
     def __init__(
         self,
-        albedo: Union[ValueMaterialProperty, TextureMaterialProperty],
-        roughness: Optional[Union[ValueMaterialProperty, TextureMaterialProperty]],
-        metallic: Optional[Union[ValueMaterialProperty, TextureMaterialProperty]],
-        roughness_metallic: Optional[Union[ValueMaterialProperty, TextureMaterialProperty]],
+        albedo: MaterialData,
+        roughness: Optional[MaterialData],
+        metallic: Optional[MaterialData],
     ):
-        self.albedo = check_material_property(albedo, 3)
-        self.metallic_roughness: Optional[Union[ValueMaterialProperty, TextureMaterialProperty]] = None
-        self.roughness: Optional[Union[ValueMaterialProperty, TextureMaterialProperty]] = None
-        self.metallic: Optional[Union[ValueMaterialProperty, TextureMaterialProperty]] = None
-
-        properties = [albedo]
-        if roughness is not None or metallic is not None:
-            if roughness is None or metallic is None:
-                raise ValueError("if roughness or metallic are not None, they both must be not None")
-            if roughness_metallic is not None:
-                raise ValueError("if roughness or metallic are not None, roughness_metallic must be None")
-            self.roughness = check_material_property(roughness, 1)
-            self.metallic = check_material_property(metallic, 1)
-            properties.append(roughness)
-            properties.append(metallic)
-        else:
-            if roughness_metallic is None:
-                raise ValueError("roughness, metallic, or roughness_metallic must be not None")
-            self.roughness_metallic = check_material_property(roughness_metallic, 2)
-            properties.append(roughness_metallic)
-
-        super().__init__(properties)
+        self.albedo = as_material_property(albedo, 3)
+        self.roughness = as_material_property(roughness, 1)
+        self.metallic = as_material_property(roughness, 1)
+        super().__init__([albedo, roughness, metallic])
